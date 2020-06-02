86e8014d382e4b049ca32503eee8f05eaab7262f
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-02 11:19:12 -0300

    Criado Rotas com Parâmetros e Query

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,52 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Teste'); 
+app.use(express.json());
 
-    response.json(['Pablo','Maria Antônia'])
+
+const users = [
+    'Pablo',
+    'Angelina Jolie',
+    'Maria Antônia'
+    ]
+
+app.get('/users', (request, response) => {
+
+    const search = String(request.query.search);
+
+    const filteredUsers =  search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers)
+})
+
+
+app.get('/users/:id', (request, response) =>{
+    console.log(`Buscando Usuário por ID`);
+
+    const id =  Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+
+
+app.post('/users',(request, response) =>{
+    const data = request.body;
+
+    console.log(`Criando um Novo User ${data}`);
+
+    const newUser = {
+        name: data.name,
+        email: data.email
+    }
+
+    return response.json(newUser)
 
 })
+
+
 
 app.listen(3333);
