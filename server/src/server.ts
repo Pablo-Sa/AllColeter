3cda942bb7acfbda222b71aaa079daee78762993
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-04 00:26:36 -0300

    SqlLite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,52 +1,9 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes'
 
 const app = express();
 
 app.use(express.json());
-
-
-const users = [
-    'Pablo',
-    'Angelina Jolie',
-    'Maria Antônia'
-    ]
-
-app.get('/users', (request, response) => {
-
-    const search = String(request.query.search);
-
-    const filteredUsers =  search ? users.filter(user => user.includes(search)) : users;
-
-    response.json(filteredUsers)
-})
-
-
-app.get('/users/:id', (request, response) =>{
-    console.log(`Buscando Usuário por ID`);
-
-    const id =  Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-
-
-app.post('/users',(request, response) =>{
-    const data = request.body;
-
-    console.log(`Criando um Novo User ${data}`);
-
-    const newUser = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(newUser)
-
-})
-
-
+app.use(routes)
 
 app.listen(3333);
