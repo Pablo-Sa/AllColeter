5ab9e31d15d779724b5c097f4f64256202a8fc83
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-12 15:58:47 -0300

    Implementado Transactions Knex

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,19 +7,54 @@ routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
 
     const serializedItems = items.map(item =>{
-
-        return { title: item.title,
+        return { 
+                 id: item.id,
+                 title: item.title,
                  image_url: `http://localhost:3333/images/${item.image}`        
                };
 
     })
 
-
     return response.json(serializedItems)
  })
 
  routes.post('/points', async(request, response) =>{
-    
+   const {
+     name,
+     email,
+     whatsapp,
+     latitude,
+     longitude,
+     city,
+     uf,
+     items
+   } = request.body;
+
+   const trx = await knex.transaction();
+
+   const insertedIds= await trx('points').insert({
+    image: 'iamge-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf    
+   });
+  
+   const point_id = insertedIds[0];
+
+   const pointItems = items.map((item_id:number) =>{
+     return {
+       item_id,
+       point_id
+     };
+   })
+
+   await trx('point_items').insert(pointItems);
+
+   return response.json({sucess: true})
 
  })
 
