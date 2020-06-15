a6cdf11ba52979cec0cc4a315f6b4149003a9977
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-15 18:48:49 -0300

    Configurações Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes'
 import path from 'path';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 app.use('/images',express.static(path.resolve(__dirname, 'images')));
 
 app.listen(3333);
