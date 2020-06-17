b566cfa55e3133eb3d8fd3bae4869e22711ae686
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-17 10:41:21 -0300

    Implementado Rotas e etc.

--- Front-End-ReactJS/src/pages/CreatePoint/index.tsx
+++ Front-End-ReactJS/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import api from '../../services/api';
 
 
 import './styles.css'
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
+
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+
+
+    useEffect(() => {
+        api.get('items').then((response) => {
+            setItems(response.data);
+        })
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -63,7 +81,7 @@ const CreatePoint = () => {
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                     <Marker  position={[-27.2092052, -49.6401092]} />
+                        <Marker position={[-27.2092052, -49.6401092]} />
                     </Map>
 
                     <div className="field-group">
@@ -87,32 +105,13 @@ const CreatePoint = () => {
                         <h2>Ítems de Coleta</h2>
                         <span>Selecione um ou mais items abaixo</span>
                     </legend>
-
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
 
                 </fieldset>
