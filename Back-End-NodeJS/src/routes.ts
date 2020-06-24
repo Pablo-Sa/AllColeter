0851e8d69360e4026df74a444ad9e3c5d1b50a12
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-06-24 11:42:07 -0300

    Implementado Celebrate no Back-End, Lib de Validação

--- Back-End-NodeJS/src/routes.ts
+++ Back-End-NodeJS/src/routes.ts
@@ -3,6 +3,7 @@ import PointsControllers from './controllers/pointsControllers';
 import ItemsControllers from './controllers/itemsControllers';
 import multer from 'multer';
 import multerConfig from '../src/config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 
 const routes = express.Router();
@@ -12,11 +13,31 @@ const upload = multer(multerConfig);
 const pointsControllers = new PointsControllers();
 const itemsControllers = new ItemsControllers();
 
+routes.get('/items', itemsControllers.index)
+
 routes.get('/points/:id', pointsControllers.show)
 routes.get('/points', pointsControllers.index)
-routes.post('/points', upload.single('image'), pointsControllers.create)
 routes.delete('/points/:id', pointsControllers.delete)
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object({
+            nome: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    },{
+     abortEarly: false   
+    }),
+    pointsControllers.create
+);
 
-routes.get('/items', itemsControllers.index)
+
 
 export default routes
